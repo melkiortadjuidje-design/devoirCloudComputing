0e3450cebafb21a29d7d2e3c9665b8faada05aa8
melkiortadjuidje-design <melkiortadjuidje@gmail.com>
2026-04-27 00:49:41 +0100

    traitement.php

--- traitement.php
+++ traitement.php
@@ -71,12 +71,12 @@ echo "Salut $nom, on a bien recu ton formulaire <br>";
 }
 
 if (isset($_FILES['avatar'])) {
-$nomFichier = $_FILES['avatar']['name'];
+$Photo = $_FILES['avatar']['name'];
 $tmp = $_FILES['avatar']['tmp_name'];
 
-move_uploaded_file($tmp, "uploads/" . $nomFichier);  
+move_uploaded_file($tmp, "uploads/" . $Photo);  
 
-echo "Photo envoyée : " . $nomFichier;
+echo "Photo envoyée : " . $Photo;
 
 }
 
