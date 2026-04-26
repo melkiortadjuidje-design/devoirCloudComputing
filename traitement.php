358826268e81c722b287d2c1c5dd9163332bcae7
melkiortadjuidje-design <melkiortadjuidje@gmail.com>
2026-04-26 15:30:28 +0100

    traitement.php

--- traitement.php
+++ traitement.php
@@ -1,23 +1,50 @@
-<?<php>
-// On vérifie que le formulaire a été enoyé
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+<?php
+
+// Vérifier si le formulaire est envoyé
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
 // On récupère les données avec $_POST['name']
-$Noms = $_POST['nom']
-$Prenom = $_POST['prenom']
-$Password = $_POST['mdp']
-$Email = $_POST['mail']
-$Age = $_POST['age']
-$Naissance = $_POST['naissance']
-$Photo = $_POST['avatar']
-$Sexe = $_POST['sexe']
-$Condition = $_POST['accepte']
-// Sécurité de base : éviter les failles XSS
-$Noms = htmlspecialchars($Noms);
-$Email = htmlspecialchars($Email)
-// Ici je fais ce que je veux avec les données
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $mdp = $_POST['mdp'];
+    $email = $_POST['mail'];
+    $age = $_POST['age'];
+    $naissance = $_POST['naissance'];
+    $sexe = $_POST['sexe'];
+    $token = $_POST['token'];
+
+    // Sécurité de base : éviter les failles XSS
+$nom = htmlspecialchars($nom);
+$email = htmlspecialchars($email)
+
+    // Checkbox
+    $accepte = isset($_POST['accepte']) ? "Oui" : "Non";
+
+    // Ici je fais ce que je veux avec les données
 echo "Salut $Noms, on a bien recu ton formulaire";
-// Exemple : Enreigistrer en BDD, envoyer un Mail, etc.
+
+    echo "Nom : $nom <br>";
+    echo "Prénom : $prenom <br>";
+    echo "Mot de passe : $mdp <br>";
+    echo "Email : $email <br>";
+    echo "Age : $age <br>";
+    echo "Naissance : $naissance <br>";
+    echo "Sexe : $sexe <br>";
+    echo "Conditions acceptées : $accepte <br>";
+
+    // Exemple : Enreigistrer en BDD, envoyer un Mail, etc.
 } else {
     echo "Acces interdit";
 }
-    <?>
+
+if (isset($_FILES['avatar'])) {
+$nomFichier = $_FILES['avatar']['name'];
+$tmp = $_FILES['avatar']['tmp_name'];
+
+move_uploaded_file($tmp, "uploads/" . $nomFichier);  
+
+echo "Photo envoyée : " . $nomFichier;
+
+}
+
+?>
